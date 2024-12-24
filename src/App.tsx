667bad9ad8b9d3d1ac0e90aea14bad2d8575b747
samy <samy@gmail.com>
2024-12-24 15:47:03 +0100

    Save current changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,34 @@
 import { useState } from "react";
 import "./App.css";
-import AddTasks from "./components/AddTasks";
+import { Tasks } from "./components/Tasks";
+import { AuthForm } from "./components/AuthForm";
+
+const USERS = [
+  "jean@gmail.com",
+  "john@gmail.com",
+  "samy@gmail.com",
+  "bryan@gmail.com",
+];
 
 function App() {
-  const [tasks, setTasks] = useState([]);
-  const handleAddTask = (newTask: string) => {
-    setTasks([...tasks, newTask]);
+  const [isLogged, setIsLogged] = useState(false);
+
+  const onLogin = (email: string) => {
+    if (USERS.includes(email)) {
+      localStorage.setItem("user", "true");
+      setIsLogged(true);
+    } else {
+      alert("User not found");
+    }
   };
 
-  return (
-    <>
-      <AddTasks onAddTask={handleAddTask} />
-      <ul>
-        {tasks.map((task, index) => (
-          <li key={index}>{task}</li>
-        ))}
-      </ul>
-    </>
-  );
+  if (isLogged) {
+    return <Tasks />;
+  }
+
+  return <AuthForm onConnectUser={onLogin} />;
 }
 
 export default App;
+
+// https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage
